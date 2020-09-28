bd881fad0c2e0f788e2e7fe1153575ada6c97bcd
RafBoerhout <70891398+RafBoerhout@users.noreply.github.com>
2020-09-28 12:07:30 +0200

    Add files via upload

---
+++ ditbenik.py
@@ -0,0 +1,22 @@
+# Doel: Vragen beatwoorden door de computer.
+
+# intput()
+# print()
+#variabelen
+
+print("Wat is jou naam?")
+naam = input()
+print(naam)
+
+print( "Wat is je achternaam?")
+anaam = input()
+print(anaam)
+
+print("Jou volledige naam is dus:")
+print ( naam + anaam)
+
+print("Waar woon je?")
+Woonplaats = input()
+print("Leuk dus je woont in") 
+print(Woonplaats)
+
