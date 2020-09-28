fecb7e135df120df99e9e4f2e08e0b09861fcd3c
RafBoerhout <70891398+RafBoerhout@users.noreply.github.com>
2020-09-28 12:34:28 +0200

    Add files via upload

--- ditbenik.py
+++ ditbenik.py
@@ -20,3 +20,25 @@ Woonplaats = input()
 print("Leuk dus je woont in") 
 print(Woonplaats)
 
+print("Werk je al")
+answer = input("zeg ja of nee : ")
+if answer == "ja":
+    print("Leuk waar werk je dan?")
+    werk = input()
+    print("leuk, ik werk bij new york pizza")
+    
+
+elif answer == "nee":
+    print("wil je ergens werken?")
+    antwoord = input("zeg ja of nee : ")
+    
+    if antwoord == "ja":
+        print("Waar wil je werken?")
+        werK = input()
+        print("leuk, ik werk bij new york pizza")
+        
+    elif antwoord == "nee":
+        print("oke maakt niet uit")
+
+
+    
